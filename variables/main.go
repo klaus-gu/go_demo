081214bd9ce86dad7943169abbdf000c09ab5748
顾悦 <guyue@ovopark.com>
2022-04-12 19:04:44 +0800

    demo

--- variables/main.go
+++ variables/main.go
@@ -1,4 +1,4 @@
-package variables
+package main
 
 import "fmt"
 
@@ -6,4 +6,12 @@ func main() {
 	var a = "init"
 	fmt.Println(a)
 
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
 }
