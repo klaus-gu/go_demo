3ba1407552d1b553c02684a7d2e81bbf11a1685e
顾悦 <guyue@ovopark.com>
2022-04-18 14:07:59 +0800

    channel

---
+++ atomic/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	var ops uint64
+	var wg sync.WaitGroup
+	for i := 1; i <= 50; i++ {
+		wg.Add(1)
+		go func() {
+			for j := 1; j <= 1000; j++ {
+				// 原子操作与非原子操作，二者计算时间差异，以及计算安全的差异
+				atomic.AddUint64(&ops, 1)
+				// ops++
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(ops)
+}
